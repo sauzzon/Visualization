27fa60875261e470f2458a4c40af94e154b6d9bb
Saujan Tiwari <saujan.tiwari@gmail.com>
2020-11-17 20:42:41 +0545

    Simple commit
    Simple commit

--- build-Visualizer-Desktop_Qt_5_15_1_MinGW_64_bit-Debug/ui_widget.h
+++ build-Visualizer-Desktop_Qt_5_15_1_MinGW_64_bit-Debug/ui_widget.h
@@ -11,6 +11,7 @@
 
 #include <QtCore/QVariant>
 #include <QtWidgets/QApplication>
+#include <QtWidgets/QComboBox>
 #include <QtWidgets/QGraphicsView>
 #include <QtWidgets/QLabel>
 #include <QtWidgets/QPushButton>
@@ -30,6 +31,8 @@ public:
     QLabel *delayLabel;
     QSlider *delaySlider;
     QPushButton *stopButton;
+    QComboBox *comboBox;
+    QLabel *label_2;
 
     void setupUi(QWidget *Widget)
     {
@@ -45,7 +48,7 @@ public:
         visualizingBackground->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
         slider = new QSlider(Widget);
         slider->setObjectName(QString::fromUtf8("slider"));
-        slider->setGeometry(QRect(1110, 70, 171, 31));
+        slider->setGeometry(QRect(1120, 170, 171, 31));
         slider->setMinimum(10);
         slider->setMaximum(250);
         slider->setSingleStep(10);
@@ -57,28 +60,37 @@ public:
         slider->setTickPosition(QSlider::NoTicks);
         label = new QLabel(Widget);
         label->setObjectName(QString::fromUtf8("label"));
-        label->setGeometry(QRect(1130, 30, 141, 21));
+        label->setGeometry(QRect(1140, 130, 141, 21));
         pushButton = new QPushButton(Widget);
         pushButton->setObjectName(QString::fromUtf8("pushButton"));
-        pushButton->setGeometry(QRect(1060, 210, 81, 51));
+        pushButton->setGeometry(QRect(1070, 310, 81, 51));
         pushButton->setFlat(false);
         resetButton = new QPushButton(Widget);
         resetButton->setObjectName(QString::fromUtf8("resetButton"));
-        resetButton->setGeometry(QRect(1150, 210, 81, 51));
+        resetButton->setGeometry(QRect(1160, 310, 81, 51));
         delayLabel = new QLabel(Widget);
         delayLabel->setObjectName(QString::fromUtf8("delayLabel"));
-        delayLabel->setGeometry(QRect(1170, 120, 81, 21));
+        delayLabel->setGeometry(QRect(1180, 220, 81, 21));
         delaySlider = new QSlider(Widget);
         delaySlider->setObjectName(QString::fromUtf8("delaySlider"));
-        delaySlider->setGeometry(QRect(1110, 150, 171, 31));
-        delaySlider->setMinimum(10);
-        delaySlider->setMaximum(2000);
-        delaySlider->setSingleStep(50);
-        delaySlider->setValue(200);
+        delaySlider->setGeometry(QRect(1120, 250, 171, 31));
+        delaySlider->setMinimum(0);
+        delaySlider->setMaximum(100);
+        delaySlider->setSingleStep(10);
+        delaySlider->setValue(10);
         delaySlider->setOrientation(Qt::Horizontal);
         stopButton = new QPushButton(Widget);
         stopButton->setObjectName(QString::fromUtf8("stopButton"));
-        stopButton->setGeometry(QRect(1240, 210, 81, 51));
+        stopButton->setGeometry(QRect(1250, 310, 81, 51));
+        comboBox = new QComboBox(Widget);
+        comboBox->addItem(QString());
+        comboBox->addItem(QString());
+        comboBox->addItem(QString());
+        comboBox->setObjectName(QString::fromUtf8("comboBox"));
+        comboBox->setGeometry(QRect(1170, 50, 111, 22));
+        label_2 = new QLabel(Widget);
+        label_2->setObjectName(QString::fromUtf8("label_2"));
+        label_2->setGeometry(QRect(1080, 50, 81, 21));
 
         retranslateUi(Widget);
 
@@ -91,8 +103,13 @@ public:
         label->setText(QCoreApplication::translate("Widget", "Number Of Elements = 125", nullptr));
         pushButton->setText(QCoreApplication::translate("Widget", "Click to Sort ", nullptr));
         resetButton->setText(QCoreApplication::translate("Widget", "Reset", nullptr));
-        delayLabel->setText(QCoreApplication::translate("Widget", "Delay = 200ms", nullptr));
+        delayLabel->setText(QCoreApplication::translate("Widget", "Delay = 10ms", nullptr));
         stopButton->setText(QCoreApplication::translate("Widget", "Stop", nullptr));
+        comboBox->setItemText(0, QCoreApplication::translate("Widget", "Selection Sort", nullptr));
+        comboBox->setItemText(1, QCoreApplication::translate("Widget", "Quick Sort", nullptr));
+        comboBox->setItemText(2, QCoreApplication::translate("Widget", "Merge Sort", nullptr));
+
+        label_2->setText(QCoreApplication::translate("Widget", "Select Algorithm", nullptr));
     } // retranslateUi
 
 };
